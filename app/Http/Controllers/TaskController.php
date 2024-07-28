39cb1106c80c723dc14e57e196f28ff2cc69c866
jinhwanchoi <an235353@gmail.com>
2024-07-29 00:17:41 +0900

    indexファイルの生成（一覧のレイアウト作成）

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Folder;
+
+class TaskController extends Controller
+{
+
+    public function index(int $id)
+    {
+        $folders = Folder::all();
+
+        return view('tasks.index',
+        [
+            'folders' => $folders,
+            'folder_id' => $id
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/2', function () {
-    return view('welcome');
-});
-Route::redirect('/', '/dashboard');
-Route::view('/dashboard', 'dashboard');
+// Route::redirect('/', '/folders/1/tasks');
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index'])->name('tasks.index');
