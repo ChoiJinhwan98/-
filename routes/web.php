ff3fb4fc94b54e3c1f04d4df1204e0214764fba8
jinhwanchoi <an235353@gmail.com>
2024-07-28 23:06:52 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/2', function () {
     return view('welcome');
 });
 Route::redirect('/', '/dashboard');
